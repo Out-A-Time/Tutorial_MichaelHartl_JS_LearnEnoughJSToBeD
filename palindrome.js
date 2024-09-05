a207fd858843441cede12a236716c08de4d15029
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2024-09-05 21:01:22 +0100

    7.2 Before prototypes

--- palindrome.js
+++ palindrome.js
@@ -5,13 +5,40 @@ function reverse(string) {
 
 console.log("Reverse:", reverse("hello"));
 
-// Returns true for a palindrome, false otherwise.
-function palindrome(string) {
-  let processedContent = string.toLowercase();
-  return processedContent === reverse(processedContent);
-}
-
 //Defines a Phrase object.
 function Phrase(content) {
   this.content = content;
+
+  //Return content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.content.toLowercase();
+  };
+
+  // Returns true for a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === reverse(this.processedContent());
+  };
 }
+
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  //Return content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.translation.toLowercase();
+  };
+}
+
+//Inheritance in practice as  TranslatedPhrase inherits methods from the partent object.
+TranslatedPhrase.prototype = new Phrase();
+
+/*
+To run file, type node in terminal, to activate REPL mode
+then run command: .load palindrome.js
+then: phrase = new Phrase("Racecar")
+then: phrase.palindrome();
+then: let frase = new TranslatedPhrase("recognize", "reconocer")
+then: frase.palindrome()
+*/
